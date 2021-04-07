b5d0e8d41fa0127e3c99ceba334f270f3b53ea58
KolyvanovA <kolyvanov.a.ak@gmail.com>
2021-04-07 13:53:27 +0300

    Initial commit

---
+++ src/com/company/Triangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Triangle extends TwoDShape {
+    String style;
+
+    double area() {
+        return width * height / 2;
+    }
+
+    void showStyle() {
+        System.out.println("Треугольник " + style);
+    }
+}
---
+++ src/com/company/TwoDShape.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TwoDShape {
+    double width;
+    double height;
+
+    void showDim (){
+        System.out.println("Ширина и высота - " + width + " и " + height);
+    }
+}
